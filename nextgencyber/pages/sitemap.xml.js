f7952da7973282ed11f9500afede1f3e0e53658b
omeshF <omeshf@gmail.com>
2026-06-01 18:48:54 +0100

    Add sitemap.xml

---
+++ nextgencyber/pages/sitemap.xml.js
@@ -0,0 +1,45 @@
+const SITE_URL = 'https://www.nextgencyber.co.uk'
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${SITE_URL}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>${SITE_URL}/net2i</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.9</priority>
+      </url>
+      <url>
+        <loc>${SITE_URL}/about</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>${SITE_URL}/contact</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+      </url>
+    </urlset>`
+}
+
+export async function getServerSideProps({ res }) {
+  const sitemap = generateSiteMap()
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return { props: {} }
+}
+
+export default function SiteMap() {
+  return null
+}
